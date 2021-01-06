c186dd57c33206eb13b4bfb1da91b65b61f446c0
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2021-01-06 01:14:36 -0400

    Iniciando Kanban

---
+++ rocketseat-version/script.js
@@ -0,0 +1,43 @@
+const cards = document.querySelectorAll('.card')
+const dropzones = document.querySelectorAll('.dropzone')
+
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('drag', drag)
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+
+}
+
+function drag() {
+
+}
+
+function dragend() {
+
+}
+
+dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+
+}
+
+function dragover() {
+
+}
+
+function dragleave() {
+
+}
+
+function drop() {
+
+}
